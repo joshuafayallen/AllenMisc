ebc70523e7d341fa7ff694c6644f2cffc3477685
joshuafayallen <jallen108@gsu.edu>
2024-05-17 09:38:18 -0400

    Update to add fonts from local files

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,9 @@
 .onAttach <- function(libname, pkgname){
   cli::cli_alert_info("Loading fonts for ggplot theme")
   showtext::showtext_auto()
-  sysfonts::font_add_google("Assistant", "Assistant")
-  sysfonts::font_add_google("Roboto Condensed", "Roboto Condensed")
-  sysfonts::font_add_google("Average", "Average")
+  sysfonts::font_add('Assistant', regular = 'Assistant-Regular.ttf')
+  sysfonts::font_add("Roboto", regular = 'Roboto-Regular.ttf')
+  sysfonts::font_add("Average", regular = 'Average-Regular.ttf')
 }
+
 
