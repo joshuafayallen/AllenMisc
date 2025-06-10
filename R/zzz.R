a45ae831fdb13c5868cad8ff37e64df06f781c89
desktop <jallen108@gsu.edu>
2025-06-10 12:52:50 -0400

    add updates to package

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 .onAttach <- function(libname, pkgname){
   showtext::showtext_auto()
-  sysfonts::font_add('Assistant', regular = 'Assistant-Regular.ttf')
-  sysfonts::font_add("Roboto", regular = 'Roboto-Regular.ttf')
-  sysfonts::font_add("Average", regular = 'Average-Regular.ttf')
-  sysfonts::font_add('Cochineal', regular = 'Cochineal-Roman.otf' )
+  sysfonts::font_add('Assistant', regular = './fonts/Assistant-Regular.ttf')
+  sysfonts::font_add("Roboto", regular = './fonts/Roboto-Regular.ttf')
+  sysfonts::font_add("Average", regular = './fonts/Average-Regular.ttf')
+  sysfonts::font_add('Cochineal', regular = './fonts/Cochineal-Roman.otf' )
 }
 
 
